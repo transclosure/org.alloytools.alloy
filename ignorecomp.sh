d9984a6e092a84a0f36c970d6827bf3dd17b3ba0
transclosure <natasha_danas@brown.edu>
2018-07-12 20:24:22 -0400

    a few helpful dev scripts

---
+++ ignorecomp.sh
@@ -0,0 +1,3 @@
+git update-index --assume-unchanged org.alloytools.alloy.core/src/main/java/edu/mit/csail/sdg/parser/CompLexer.java
+git update-index --assume-unchanged org.alloytools.alloy.core/src/main/java/edu/mit/csail/sdg/parser/CompParser.java
+git update-index --assume-unchanged org.alloytools.alloy.core/src/main/java/edu/mit/csail/sdg/parser/CompSym.java
