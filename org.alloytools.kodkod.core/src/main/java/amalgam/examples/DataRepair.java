06917758c361feca9e75affe62a5e946a176932b
Tim Nelson <tn@cs.brown.edu>
2018-11-15 09:58:47 -0500

    Revert "naive cegis loop added to all current kodkod examples. run the evaluation script to see the current printout. i still need to add the refinement part of the loop, so right now, it fails to find a configuration because its not learning at all."
    This reverts commit f1654ee7329fcc2670f343c2fbe302af3fe5bba5.

--- org.alloytools.kodkod.core/src/main/java/amalgam/examples/DataRepair.java
+++ org.alloytools.kodkod.core/src/main/java/amalgam/examples/DataRepair.java
@@ -1,4 +1,24 @@
-package amalgam.examples;
+package amalgam.examples;/*
+ * Kodkod -- Copyright (c) 2005-2008, Emina Torlak
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -55,14 +75,6 @@ public final class DataRepair implements KodkodExample {
     }
 
     @Override
-    public Formula refine(Formula current, Instance refinement)  {
-        return Formula.TRUE;
-    }
-
-    @Override
-    public Bounds restrict(Bounds current, Instance restriction) { return current; }
-
-    @Override
     public Formula formula() {
         final List<Formula> formulas = new ArrayList<>();
         final Variable n = Variable.unary("n");
@@ -77,7 +89,7 @@ public final class DataRepair implements KodkodExample {
     }
 
     @Override
-    public Map<Relation,TupleSet> target(Bounds bounds) {
+    public Map<Relation,TupleSet> targets(Bounds bounds) {
         Map<Relation,TupleSet> targets = new LinkedHashMap<>();
         List<Tuple> colors = new ArrayList<>();
         int n = bounds.upperBound(node).size();
--- org.alloytools.kodkod.core/src/main/java/amalgam/examples/KodkodExample.java
+++ org.alloytools.kodkod.core/src/main/java/amalgam/examples/KodkodExample.java
@@ -3,17 +3,11 @@ package amalgam.examples;
 import kodkod.ast.Formula;
 import kodkod.ast.Relation;
 import kodkod.instance.Bounds;
-import kodkod.instance.Instance;
 import kodkod.instance.TupleSet;
 import java.util.Map;
 
 public interface KodkodExample {
-    // Kodkod
     Bounds bounds(int n);
     Formula formula();
-    // CEGIS
-    Formula refine(Formula current, Instance refinement);
-    Bounds restrict(Bounds current, Instance restriction);
-    // Target Oriented
-    Map<Relation,TupleSet> target(Bounds bounds);
+    Map<Relation,TupleSet> targets(Bounds bounds);
 }
